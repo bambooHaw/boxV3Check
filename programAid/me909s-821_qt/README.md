0116bbe90fd6adeeb20dce1b09c6930fceb07318
Henry <xiansheng929@163.com>
2018-08-30 19:00:12 +0800

    delete all

--- programAid/me909s-821_qt/README.md
+++
@@ -1,2 +0,0 @@
-# me909s-821
-Qt codes for HUAWEI ME909S-821 LTE net dialing
--- programAid/me909s-821_qt/threads.h
+++
@@ -1,7 +0,0 @@
-#ifndef THREADS_H
-#define THREADS_H
-
-extern void* threadDialStartRoutine(void* args);
-
-#endif // THREADS_H
-
